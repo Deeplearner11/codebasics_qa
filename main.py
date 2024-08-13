0ff9e328747cafe48f57acb64f001f169900f1c1
Deeplearner11 <87230145+Deeplearner11@users.noreply.github.com>
2024-08-14 00:00:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_helper import get_qa_chain, create_vector_db
 
-st.title("Codebasics Q&A 🌱")
+st.title("Codebasics QA")
 btn = st.button("Create Knowledgebase")
 if btn:
     create_vector_db()
